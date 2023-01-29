ea2e5141eb7198a0475cd25a3913078c3a48a1da
Valpire <115499080+Valpire@users.noreply.github.com>
2023-01-29 19:46:47 +0300

    Add files via upload

---
+++ лабораторная 3/main.py
@@ -0,0 +1,70 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name: str) -> None:
+        self._name = name
+
+    @property
+    def author(self) -> str:
+        return self._author
+
+    @author.setter
+    def author(self, author:str) -> None:
+        self._author = author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook:
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name,author)
+        self.pages = pages
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, pages: int) -> None:
+        if not isinstance(pages, int):
+            raise TypeError('Число страниц должно быть целым числом')
+        if pages < 1:
+            raise ValueError('Число страниц должно быть больше 0')
+        self._pages = pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, duration: float) -> None:
+        if not isinstance(duration, float):
+            raise TypeError('Продолжительность аудиокниги должна быть вещественным числом')
+        if duration <= 0:
+            raise ValueError('Продолжительность аудиокниги должна быть больше 0')
+        self._duration = duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
